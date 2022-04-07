03705f7b939825799945ac93b9eb720463085625
Allan Le Lay <allan.lelay@gmail.com>
2022-04-08 00:45:52 +0200

    starting project, adding a quickversion of index

---
+++ lelaydepannage/resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts/template')
+@section('title','Accueil - LLD')
+@section('content')
+    <div id="brandLogo">
+        <h1>LLD</h1>
+        <h2>image du logo</h2>
+    </div>
+@endsection
---
+++ lelaydepannage/resources/views/layouts/template.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+    <link href="{{ asset('stylesheet.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <nav>
+        <a href="#">Accueil</a>
+        <a href="#">Mes services</a>
+        <a href="#">Prise de rendez-vous</a>
+        <a href="#">Véhicules disponibles</a>
+        <a href="#">Contact</a>
+        <div class="animation start-home"></div>
+    </nav>
+<div class="container">
+@yield('content')
+
+</div>
+</body>
+</html>
