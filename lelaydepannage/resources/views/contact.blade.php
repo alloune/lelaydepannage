9ad57bf266872d34cb65cd41b8b026639ff3ba21
Allan Le Lay <allan.lelay@gmail.com>
2022-04-10 10:46:48 +0200

    Contact: Update, more simple. Need to improve info side

--- lelaydepannage/resources/views/contact.blade.php
+++ lelaydepannage/resources/views/contact.blade.php
@@ -5,33 +5,45 @@
     <div id="contact">
         <div class="contactForm">
             <div class="inputForm">
-                <h3>Besoin d'un renseignement ? D'être accompagné dans le recyclage de votre épave ?</h3>
+                <h3>Besoin d'un renseignement ? </h3>
                 <form method="post" action="{{ route('messages.store') }}">
                     @method('post')
                     @csrf
-                <label>Prénom</label>
-                <input type="text" placeholder="Votre prénom" name="firstName">
-                <label>Nom</label>
-                <input type="text" placeholder="Votre nom" name="lastName">
-                <label>E-mail</label>
-                <input type="email" placeholder="recup@gmail.com" name="email">
-                <label>Sujet de votre message</label>
-                <select name="subject">
-                    <option>Dépannage</option>
-                    <option>Enlèvement de ferraille</option>
-                    <option>Enlèvement de véhicule</option>
-                    <option>Certificat de destruction</option>
-                    <option>Autre</option>
-                </select>
-                <label>Votre message</label>
-                <textarea name="coreMessage" placeholder="Écrivez votre message ici ..."></textarea>
-                <input type="submit">
+                    <div id="contact1">
+                        <label>Prénom</label>
+                        <input type="text" placeholder="Votre prénom" name="firstName">
+                        <label>Nom</label>
+                        <input type="text" placeholder="Votre nom" name="lastName">
+                        <label>E-mail</label>
+                        <input type="email" placeholder="recup@gmail.com" name="email">
+                        <label>Sujet de votre message</label>
+                        <select name="subject">
+                            <option>Dépannage</option>
+                            <option>Enlèvement de ferraille</option>
+                            <option>Enlèvement de véhicule</option>
+                            <option>Certificat de destruction</option>
+                            <option>Autre</option>
+                        </select>
+                    </div>
+                    <div id="contact2">
+                        <label>Votre message</label>
+                        <textarea name="coreMessage" placeholder="Écrivez votre message ici ..."></textarea>
+                        <input class="submit" type="submit">
+                    </div>
                 </form>
 
 
             </div>
-            <div>
-                <img src="{{ asset('contact_img.png') }}">
+            <div class="infoContact">
+                <h3>Adresse</h3>
+                <p>550 Chemin des matières</p>
+                <p>38260 Champier</p>
+                <h3>Tel</h3>
+                <p>07-88-59-69-66</p>
+                <h3>Mail</h3>
+                <p>lelay.jordan@outlook.fr</p>
+                <h3>Siret</h3>
+                <p>Tu me le fileras j'en sais rien</p>
             </div>
         </div>
 
