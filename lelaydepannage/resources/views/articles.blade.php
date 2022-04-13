e8bc115c1bbba776f2ab135d401d7ae3aa42fe5c
Allan Le Lay <allan.lelay@gmail.com>
2022-04-13 23:20:03 +0200

    ?

--- lelaydepannage/resources/views/articles.blade.php
+++ lelaydepannage/resources/views/articles.blade.php
@@ -2,8 +2,15 @@
 @section('title','Accueil - LLD')
 @section('content')
 
-    <div id="products">
+    <section id="" class="py-5 text-center container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="fw-light">Tous les véhicules disponibles</h1>
+                <p class="lead text-muted">Retrouvez ici tous mes véhicules disponible à la vente. Vous retrouverez tous
+                    les détails en cliquant sur la description</p>
 
+            </div>
 
-    </div>
+        </div>
+    </section>
 @endsection
--- lelaydepannage/resources/views/layouts/template.blade.php
+++ lelaydepannage/resources/views/layouts/template.blade.php
@@ -5,35 +5,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="{{ asset('stylesheet.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
 </head>
 <body>
-
-    <nav>
-{{--        <img class="headerLogo" src="{{ asset('logo.png') }}" alt="logo LLD">--}}
-        <a href="{{ route('index') }}">Accueil</a>
-        <a href="{{ route('services') }}">Mes services</a>
-        <a href="{{ route('rdv') }}">Prise de rendez-vous</a>
-        <a href="{{ route('vehicule') }}">Véhicules disponibles</a>
-        <a href="{{ route('contact') }}">Contact</a>
-        <div class="animation start-home"></div>
-    </nav>
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+    <ul class="navbar-nav">
+        <img src="{{ asset('logo.png') }}">
+        <li class="nav-item active">
+            <a class="nav-link" href="{{ route('index') }}">Accueil</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="{{ route('services') }}">Mes services</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="{{ route('rdv') }}">Prendre rendez-vous</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('vehicule') }}">Véhicules disponibles</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+        </li>
+    </ul>
+</nav>
 @yield('contact')
 <div class="container">
-@yield('content')
+    @yield('content')
 
 </div>
-    <footer>
+<footer>
 
-        <div class="favicon">
-            <img src="{{ asset('facebook.png') }}">
-            <img src="{{ asset('instagram.png') }}">
-            <img src="{{ asset('outlook.png') }}">
+    <div class="favicon">
+        <img src="{{ asset('facebook.png') }}">
+        <img src="{{ asset('instagram.png') }}">
+        <img src="{{ asset('outlook.png') }}">
 
-        </div>
-        <p> Le Lay Dev.Web Copyright 2022</p>
-    </footer>
+    </div>
+    <p> Le Lay Dev.Web Copyright 2022</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 </body>
 </html>
