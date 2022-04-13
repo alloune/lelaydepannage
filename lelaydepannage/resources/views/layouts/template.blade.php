4645792c5f8bbc061461a632e2ec5bcf2bb5c3e5
Allan Le Lay <allan.lelay@gmail.com>
2022-04-14 00:35:54 +0200

    trying to make everything clean, but it's hard for now ...

--- lelaydepannage/resources/views/layouts/template.blade.php
+++ lelaydepannage/resources/views/layouts/template.blade.php
@@ -17,7 +17,7 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white ">
 
 
-    <div class="col-4 collapse d-flex justify-content-center navbar-collapse " id="navbarNav">
+    <div class="col d-flex justify-content-center  " id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
                 <a class="nav-link text-dark" href="{{ route('services') }}">Mes services</a>
@@ -29,11 +29,11 @@
 
         </ul>
     </div>
-    <div class="col-4 d-flex justify-content-center nav navbar-brand">
-    <a class="d-flex justify-content-center" href="{{ route('index') }}" ><img src="{{ asset('logo.png') }}"></a>
+    <div class="col d-flex justify-content-center nav navbar-brand">
+        <a class="d-flex justify-content-center" href="{{ route('index') }}"><img src="{{ asset('logo.png') }}"></a>
     </div>
 
-    <div class="col-4 d-flex justify-content-center collapse navbar-collapse ">
+    <div class="col d-flex justify-content-center ">
         <ul class="navbar-nav">
             <li class="nav-item active">
                 <a class="nav-link text-dark" href="{{ route('rdv') }}">Prise de rendez-vous</a>
@@ -46,27 +46,26 @@
         </ul>
     </div>
 </nav>
+<div class="container-fluid">
 
-@yield('contact')
-<div class="container">
     @yield('content')
 
 </div>
-<footer>
+<footer class="footer">
+    <div class="col row">
+        <p class="col">1</p>
+        <p class="col">2</p>
+        <p class="col">3</p>
 
-    <div class="favicon">
-        <img src="{{ asset('facebook.png') }}">
-        <img src="{{ asset('instagram.png') }}">
-        <img src="{{ asset('outlook.png') }}">
 
     </div>
-    <p> Le Lay Dev.Web Copyright 2022</p>
+    <div class="col">
+       <p class="col">4</p>
+    </div>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
-
-
 </body>
 </html>
