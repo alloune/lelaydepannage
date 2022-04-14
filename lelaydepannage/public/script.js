032a735974a9738606627d9742d431d972b4f0b0
allan <allan.lelay@gmail.com>
2022-04-14 16:40:11 +0200

    finishing validate for add vehicule

--- lelaydepannage/public/script.js
+++ lelaydepannage/public/script.js
@@ -5,66 +5,64 @@ function validateDataVehicule() {
 
     let articleToAdd =
         {
-        'brand': '',
-        'model': '',
-        'engine': '',
-        'fuel': '',
-        'km': '',
-        'price': '',
-        'photo': '',
-
+            'brand': '',
+            'model': '',
+            'engine': '',
+            'fuel': '',
+            'km': '',
+            'price': '',
+            'photo': '',
         }
+    let validateClientInput = [
+        {
+            'id': 'brand',
+            'phrase': 'La marque n\'est pas renseignée'
+        },
+        {
+            'id': 'model',
+            'phrase': 'Le modèle n\'est pas renseigné'
+        },
+        {
+            'id': 'engine',
+            'phrase': 'Le type de moteur n\'est pas renseigné'
+        },
+        {
+            'id': 'fuel',
+            'phrase': 'Le carburant n\'est pas renseigné'
+        },
+        {
+            'id': 'km',
+            'phrase': 'Le kilométrage n\'est pas renseigné'
+        },
+        {
+            'id': 'price',
+            'phrase': 'Le prix n\'est pas renseignée'
+        },
+        {
+            'id': 'photo',
+            'phrase': 'Le lien de la photo n\'est pas renseignée'
+        }
+    ]
+
 
 
     articleToAdd.brand = $("#brandInput").val()
-    articleToAdd.model= $("#modelInput").val()
-    articleToAdd.engine= $("#engineInput").val()
-    articleToAdd.fuel= $("#fuelInput").val()
-    articleToAdd.km= $("#kmInput").val()
-    articleToAdd.price= $("#priceInput").val()
-    articleToAdd.photo= $("#photoInput").val()
+    articleToAdd.model = $("#modelInput").val()
+    articleToAdd.engine = $("#engineInput").val()
+    articleToAdd.fuel = $("#fuelInput").val()
+    articleToAdd.km = $("#kmInput").val()
+    articleToAdd.price = $("#priceInput").val()
+    articleToAdd.photo = $("#photoInput").val()
 
     console.log(articleToAdd)
+    console.log(validateClientInput)
     for (const [key, value] of Object.entries(articleToAdd)) {
-        console.log(`${key}: ${value}`);
+        if(!value){
+            let toto = validateClientInput.find(element => element.id == key)
+            $('#div'+key+'Input').append(`<div class="alert alert-warning">`+toto.phrase+`</div>`)
+        }
     }
-    // Object.values(articleToAdd).forEach(element =>
-    //     if(!element){
-    //         console.log("toto")
-    //     }
 
-
-    // if (!$('#brandInput').val() && !$('#modelInput').val() && !$('#engineInput').val() && !$('#fuelInput').val() && !$('#kmInput').val() && !$('#priceInput').val() && !$('#photoInput').val()) {
-    //     alert('Vous n\'avez rien entré !')
-    // }
-    // else {
-    //
-    //     if (!$('#brandInput').val()) {
-    //         $('#divbrandInput').append(`<div class="alert alert-warning">La marque est vide ...</div>`)
-    //     }
-    //     if (!$('#modelInput').val()) {
-    //         $('#divmodelInput').append(`<div class="alert alert-warning">Le model est vide ...</div>`)
-    //     }
-    //     if (!$('#engineInput').val()) {
-    //         $('#divengineInput').append(`<div class="alert alert-warning">Le type de moteur est vide ...</div>`)
-    //     }
-    //     if (!$('#fuelInput').val()) {
-    //         $('#divfuelInput').append(`<div class="alert alert-warning">Le carburant est vide ...</div>`)
-    //     }
-    //     if (!$('#kmInput').val()) {
-    //         $('#divkmInput').append(`<div class="alert alert-warning">Le kilometrage est vide ...</div>`)
-    //     }
-    //     if (!$('#priceInput').val()) {
-    //         $('#divpriceInput').append(`<div class="alert alert-warning">Le prix est vide ...</div>`)
-    //     }
-    //     if (!$('#photoInput').val()) {
-    //         $('#divphotoInput').append(`<div class="alert alert-warning">L'image est vide ...</div>`)
-    //     }
-    //
-    //
-    //
-    //
-    // }
 
 
 }
