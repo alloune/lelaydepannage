3468031df7c3cd6c94287b994fe40c1fb0180bb0
Allan Le Lay <allan.lelay@gmail.com>
2022-04-23 12:14:35 +0200

    changing message for empty articles

--- lelaydepannage/resources/views/articles.blade.php
+++ lelaydepannage/resources/views/articles.blade.php
@@ -8,7 +8,7 @@
         <div class="album py-5 bg-light">
             <div class="container">
                 <div class="row">
-
+                    @if(count($vehicules) > 0)
                     @foreach($vehicules as $vehicule)
 
                         <div class="col-md-4">
@@ -29,7 +29,9 @@
                             </div>
                         </div>
                     @endforeach
-
+                    @else
+                    <h3>Désolé, actuellement aucun véhicule en vente</h3>
+                    @endif
                 </div>
             </div>
         </div>
