6cb4fda6f4a3c2f3b888d7923a1bac12ef836769
Allan Le Lay <allan.lelay@gmail.com>
2022-04-30 22:58:16 +0200

    update for mobile

--- lelaydepannage/resources/views/services.blade.php
+++ lelaydepannage/resources/views/services.blade.php
@@ -3,80 +3,32 @@
 @section('content')
 
 
-    <div class="services container ">
-        <div class="row">
-            <div class="col col-6">
-                <img class="img-fluid " src="{{ asset('3cars.jpg') }}">
-            </div>
-            <div class="col col-6">
-                <h2>Toto</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequatur est ipsum modi molestias
-                    nobis perspiciatis? Dolorem ea explicabo tenetur veritatis. Atque et harum iure laudantium molestias
-                    officia rem vero.</p>
-            </div>
+    <div class="services">
+        <div>
+            <img src="{{ asset('delivery.jpg') }}">
+            <h3 style="text-align: center">Livraison de véhicules</h3>
+            <p>Suite à l'achat d'un véhicule, il n'est pas toujours simple de le faire acheminer chez sois.</p>
+            <br/>Pour palier à cette difficulté, je met à disposition mon savoir faire et mon matériel afin de livrer
+            dans les meilleures conditions votre nouveau véhicule.
         </div>
-        <div class="row">
-            <div class="col col-6">
-                <h2>lorem</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laborum nemo repellat, sequi soluta
-                    veniam. Ab, accusamus aperiam ipsa natus omnis optio, placeat quas quod quos recusandae repellendus
-                    repudiandae ullam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at beatae distinctio
-                    dolor dolores</p>
-                <h3>Petit titre</h3>
-                <p>
-                    earum, excepturi exercitationem expedita fugiat laboriosam magni odit quidem ratione
-                    repellendus reprehenderit ullam ut. Facere, reprehenderit?</p>
-            </div>
-            <div class="col col-6"><img class="img-fluid" src="{{ asset('3cars.jpg') }}"></div>
+        <div style="background-color: #FF9000">
+            <img src="{{ asset('pickup.jpg') }}">
+            <h3 style="text-align: center">Enlèvement d'encombrants</h3>
+            <p>Il reste souvent beaucoup de déchet, de matériel dont nous n'avons plus l'utilité après l'achat d'un
+                bien, d'un fond de commerce etc.
+                <br/>Afin de vous faciliter la tâche dans votre transition, je vous viens en aide pour débarrasser tous
+                ses élements dont vous ne voulez plus et qui prennent une place immense.
         </div>
-        <div class="row">
-            <div class="col col-6"><img class="img-fluid " src="{{ asset('3cars.jpg') }}"></div>
-            <div class="col col-6">
-                <h4>Blabla</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, architecto beatae culpa debitis,
-                    ducimus et eveniet exercitationem expedita explicabo minus nam nobis omnis quam qui quod repellendus
-                    suscipit, voluptatem voluptatum!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi doloremque ea esse fuga, id
-                    impedit labore laudantium, natus, provident quam quas ratione recusandae similique suscipit tempore
-                    unde veritatis voluptas!</p>
-                <h2>autre titre</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at dolore facere fugit iste odit
-                    quis reprehenderit tenetur vero voluptatum? Amet dicta doloremque ex exercitationem ipsum itaque
-                    perferendis similique ullam!</p>
-                <h2>Encore un tiens</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid aspernatur at corporis
-                    cum ea eum ex illo laboriosam molestiae molestias natus officiis quia, quibusdam ratione
-                    repellendus, temporibus ullam ut! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                    minus nobis repellat tempora. Magni perspiciatis provident recusandae voluptas voluptatibus! Aliquam
-                    aliquid eligendi harum hic ipsum labore nostrum praesentium quos reiciendis. Lorem ipsum dolor sit
-                    amet, consectetur adipisicing elit. Aliquid animi aperiam blanditiis ducimus ea eligendi eveniet
-                    excepturi explicabo fuga in iusto magni nulla, officia recusandae sed similique sunt unde ut.</p>
-            </div>
+        <div>
+            <img src="{{ asset('destruction.jpg') }}">
+            <h3 style="text-align: center">Destruction d'épave</h3>
+            <p>Ma société étant agréee pour la destruction d'épave, je viens chez vous récuperer le véhicule, j'établis
+                alors un CERFA de destruction que vous devrez fournir à votre assurance où pour bénéficier de la prime à
+                descrution lors de l'achat d'un nouveau véhicule.
+                <br/>Peut importe l'état du véhicule, je viendrais vous en débarasser. Seul impératif, pouvoir finir la
+                carte grise.
         </div>
-        <div class="row">
-            <div class="col col-6">
-                <h4>Blabla</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, architecto beatae culpa debitis,
-                    ducimus et eveniet exercitationem expedita explicabo minus nam nobis omnis quam qui quod repellendus
-                    suscipit, voluptatem voluptatum!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi doloremque ea esse fuga, id
-                    impedit labore laudantium, natus, provident quam quas ratione recusandae similique suscipit tempore
-                    unde veritatis voluptas!</p>
-                <h2>autre titre</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid at dolore facere fugit iste odit
-                    quis reprehenderit tenetur vero voluptatum? Amet dicta doloremque ex exercitationem ipsum itaque
-                    perferendis similique ullam!</p>
-                <h2>Encore un tiens</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid aspernatur at corporis
-                    cum ea eum ex illo laboriosam molestiae molestias natus officiis quia, quibusdam ratione
-                    repellendus, temporibus ullam ut! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                    minus nobis repellat tempora. Magni perspiciatis provident recusandae voluptas voluptatibus! Aliquam
-                    aliquid eligendi harum hic ipsum labore nostrum praesentium quos reiciendis. Lorem ipsum dolor sit
-                    amet, consectetur adipisicing elit. Aliquid animi aperiam blanditiis ducimus ea eligendi eveniet
-                    excepturi explicabo fuga in iusto magni nulla, officia recusandae sed similique sunt unde ut.</p>
-            </div>
-            <div class="col col-6"><img class="img-fluid" src="{{ asset('3cars.jpg') }}"></div>
-        </div>
+
 
     </div>
 @endsection
