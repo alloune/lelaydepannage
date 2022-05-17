d917ebcf27b79ea4eb017e1b802b3b0ddfba2232
Allan LE LAY <allan.le-lay@Digital-Grenoble.local>
2022-05-17 11:09:46 +0200

    update index display

--- lelaydepannage/resources/views/index.blade.php
+++ lelaydepannage/resources/views/index.blade.php
@@ -1,62 +1,67 @@
 @extends('layouts/template')
 @section('title','Accueil - LLD')
 @section('content')
-{{--    <div class="indexBody">--}}
-{{--        <img class="" src="{{ asset('depannage_vieille_peugeot.jpg') }}">--}}
+    <div class="indexBody">
+        <img class="" src="{{ asset('depannage_vieille_peugeot.jpg') }}">
 
-{{--        <div class="indexCard">--}}
-{{--            <div class="card"><h1>Gratuit</h1></div>--}}
-{{--            <div class="card"><h1>7/7j</h1></div>--}}
+        <div class="indexCard">
+            <div class="card"><h1>Gratuit</h1></div>
+            <div class="card"><h1>7/7j</h1></div>
 
-{{--        </div>--}}
-{{--        <div class="secondBlock">--}}
-{{--            <h2>Le cycle de vie d'un véhicule</h2>--}}
-{{--            <p>Actuellement, on estime à 38,2 millions de voitures en circulation en france. Chaque véhicules est--}}
-{{--                composé en moyenne de 711kg de matériaux ferreux ou en acier, ainsi que de 100 kilos de--}}
-{{--                fontes/alliages. Soit 810 kilos recyclable sur un poids total d'environs 1 tonne.--}}
-{{--                Ce qui fait des véhicules une des productions humaines les mieux recyclables, 81% au total.<br>--}}
-{{--                La durée de vie d'une voiture ne se compte pas forcement en année, mais plûtot en kilomètre parcouru.--}}
-{{--                Cette--}}
-{{--                durée de vie oscille entre 200 000 km et 400 000km pour les voitures les plus resistantes.--}}
-{{--                Pour plus de renseignement, n'hésitez pas à visiter <a--}}
-{{--                    href="https://blog.vivacar.fr/quelle-est-la-duree-de-vie-dune-voiture/">ce site</a></p>--}}
-{{--        </div>--}}
-{{--        <div class="thirdBlock">--}}
-{{--            <h2>Les autres sources de métaux recyclable</h2>--}}
-{{--            <p>Il est également possible de récupérer des materiaux ferreux d'autre origine. On peut penser par exemple--}}
-{{--                au bardage--}}
-{{--                d'une ancienne usine, du matériel professionnel comme des équipements de cuisines, des chauffe-eaux--}}
-{{--                etc.</p>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="services">--}}
-{{--        <div>--}}
-{{--            <img src="{{ asset('delivery.jpg') }}">--}}
-{{--            <h3 style="text-align: center">Livraison de véhicules</h3>--}}
-{{--            <p>Suite à l'achat d'un véhicule, il n'est pas toujours simple de le faire acheminer chez sois.</p>--}}
-{{--            <br/>Pour palier à cette difficulté, je met à disposition mon savoir faire et mon matériel afin de livrer--}}
-{{--            dans les meilleures conditions votre nouveau véhicule.--}}
-{{--        </div>--}}
-{{--        <div style="background-color: #FF9000">--}}
-{{--            <img src="{{ asset('pickup.jpg') }}">--}}
-{{--            <h3 style="text-align: center">Enlèvement d'encombrants</h3>--}}
-{{--            <p>Il reste souvent beaucoup de déchet, de matériel dont nous n'avons plus l'utilité après l'achat d'un--}}
-{{--                bien, d'un fond de commerce etc.--}}
-{{--                <br/>Afin de vous faciliter la tâche dans votre transition, je vous viens en aide pour débarrasser tous--}}
-{{--                ses élements dont vous ne voulez plus et qui prennent une place immense.--}}
-{{--        </div>--}}
-{{--        <div>--}}
-{{--            <img src="{{ asset('destruction.jpg') }}">--}}
-{{--            <h3 style="text-align: center">Destruction d'épave</h3>--}}
-{{--            <p>Ma société étant agréee pour la destruction d'épave, je viens chez vous récuperer le véhicule, j'établis--}}
-{{--                alors un CERFA de destruction que vous devrez fournir à votre assurance où pour bénéficier de la prime à--}}
-{{--                descrution lors de l'achat d'un nouveau véhicule.--}}
-{{--                <br/>Peut importe l'état du véhicule, je viendrais vous en débarasser. Seul impératif, pouvoir finir la--}}
-{{--                carte grise.--}}
-{{--        </div>--}}
+        </div>
+        <div class="secondBlock">
+            <h2>Le cycle de vie d'un véhicule</h2>
+            <p>Actuellement, on estime à 38,2 millions de voitures en circulation en france. Chaque véhicules est
+                composé en moyenne de 711kg de matériaux ferreux ou en acier, ainsi que de 100 kilos de
+                fontes/alliages. Soit 810 kilos recyclable sur un poids total d'environs 1 tonne.
+                Ce qui fait des véhicules une des productions humaines les mieux recyclables, 81% au total.<br>
+                La durée de vie d'une voiture ne se compte pas forcement en année, mais plûtot en kilomètre parcouru.
+                Cette
+                durée de vie oscille entre 200 000 km et 400 000km pour les voitures les plus resistantes.
+                Pour plus de renseignement, n'hésitez pas à visiter <a
+                    href="https://blog.vivacar.fr/quelle-est-la-duree-de-vie-dune-voiture/">ce site</a></p>
+        </div>
+        <div class="thirdBlock">
+            <h2>Les autres sources de métaux recyclable</h2>
+            <p>Il est également possible de récupérer des materiaux ferreux d'autre origine. On peut penser par exemple
+                au bardage
+                d'une ancienne usine, du matériel professionnel comme des équipements de cuisines, des chauffe-eaux
+                etc.</p>
+        </div>
+        <div class="services">
+            <div>
+                <img src="{{ asset('delivery.jpg') }}">
+                <h3 style="text-align: center">Livraison de véhicules</h3>
+                <p>Suite à l'achat d'un véhicule, il n'est pas toujours simple de le faire acheminer chez sois.</p>
+                <br/>Pour palier à cette difficulté, je met à disposition mon savoir faire et mon matériel afin de
+                livrer
+                dans les meilleures conditions votre nouveau véhicule.
+            </div>
+            <div style="background-color: #FF9000">
+                <img src="{{ asset('pickup.jpg') }}">
+                <h3 style="text-align: center">Enlèvement d'encombrants</h3>
+                <p>Il reste souvent beaucoup de déchet, de matériel dont nous n'avons plus l'utilité après l'achat d'un
+                    bien, d'un fond de commerce etc.
+                    <br/>Afin de vous faciliter la tâche dans votre transition, je vous viens en aide pour débarrasser
+                    tous
+                    ses élements dont vous ne voulez plus et qui prennent une place immense.
+            </div>
+            <div>
+                <img src="{{ asset('destruction.jpg') }}">
+                <h3 style="text-align: center">Destruction d'épave</h3>
+                <p>Ma société étant agréee pour la destruction d'épave, je viens chez vous récuperer le véhicule,
+                    j'établis
+                    alors un CERFA de destruction que vous devrez fournir à votre assurance où pour bénéficier de la
+                    prime à
+                    descrution lors de l'achat d'un nouveau véhicule.
+                    <br/>Peut importe l'état du véhicule, je viendrais vous en débarasser. Seul impératif, pouvoir finir
+                    la
+                    carte grise.
+            </div>
 
 
-{{--    </div>--}}
+        </div>
+    </div>
 
 
 @endsection
