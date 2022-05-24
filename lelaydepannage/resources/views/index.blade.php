3d5b681b90af53bbc0fe475ff41c4f18f50163f5
Allan LE LAY <allan.le-lay@Digital-Grenoble.local>
2022-05-24 13:28:28 +0200

    update desktop part*

--- lelaydepannage/resources/views/index.blade.php
+++ lelaydepannage/resources/views/index.blade.php
@@ -1,25 +1,81 @@
 @extends('layouts/template')
 @section('title','Accueil - LLD')
 @section('content')
+
     <div class="sentenceAcc">
         <img src="{{ asset('car1Carou.svg') }}">
         <h4>Besoin d'un ferrailleur ?<br/> Nous vous proposons des services spécifiques à vos besoins !</h4>
     </div>
+
+
+
+    <div class="carouselIndex">
+        <div class="orangeBlock">
+            <img src="{{ asset('7_7Wide.svg') }}">
+        </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('carou1.png') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('carou2.png') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('carou3.png') }}" class="d-block w-100" alt="...">
+            </div><div class="carousel-item">
+                <img src="{{ asset('carou4.png') }}" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+        <div class="orangeBlock">
+            <h4>Besoin d’un ferrailleur ?
+                Nous vous proposons des services spécifiques à vos besoins !</h4>
+        </div>
+    </div>
+
+
+
     <h4 class="nosServices">Nos Services</h4>
     <div class="services">
     </div>
     <div class="listOfServices">
         <div class="icon">
-            <h4>Depannage | Livraison</h4>
+            <h4>Dépannage | Livraison</h4>
             <img src="{{ asset('dep.svg') }}">
+            <div class="textDesciption">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consectetur consequuntur, ea earum eius est, eum labore laborum laudantium magni maxime molestiae, molestias natus nesciunt optio praesentium quaerat tempora voluptatum.</p>
+
+            </div>
         </div>
         <div>
             <img src="{{ asset('destr.svg') }}">
+            <div class="textDesciption">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consectetur consequuntur, ea earum eius est, eum labore laborum laudantium magni maxime molestiae, molestias natus nesciunt optio praesentium quaerat tempora voluptatum.</p>
+
+            </div>
         </div>
         <div class="icon">
 
             <h4>Enlèvement d'encombrant</h4>
             <img src="{{ asset('enc.svg') }}">
+            <div class="textDesciption">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consectetur consequuntur, ea earum eius est, eum labore laborum laudantium magni maxime molestiae, molestias natus nesciunt optio praesentium quaerat tempora voluptatum.</p>
+            </div>
         </div>
     </div>
 
--- lelaydepannage/resources/views/layouts/template.blade.php
+++ lelaydepannage/resources/views/layouts/template.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title')</title>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-MLRGY1F6RD"></script>
-    <script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    <script>
         window.dataLayer = window.dataLayer || [];
 
         function gtag() {
@@ -38,6 +38,7 @@
         <li><a href="{{ route('contact') }}">Contact</a></li>
     </ul>
 </div>
+
 <div class="navBar">
     <div class="imagePlace">
         <img src="{{ asset('logo.png') }}">
@@ -50,17 +51,18 @@
           </span>
     </a>
 </div>
-<div id="wideScreenNav">
-    <div class="imagePlace">
-        <img src="{{ asset('logo.png') }}">
-    </div>
-    <div class="link">
-        <a href="{{ route("index") }}">Accueil</a>
-        <a href="{{ route("articles.index") }}">Mes véhicules</a>
-        <a href="{{ route("contact") }}">Contact</a>
-    </div>
 
+<div class="fullWidthMenu">
+    <div id="wideScreenNav">
+        <div class="imagePlace">
+            <img src="{{ asset('logo.png') }}">
+        </div>
+        <div class="link">
+            <a href="{{ route("articles.index") }}">Véhicules</a>
+            <a href="{{ route("contact") }}">Contact</a>
+        </div>
 
+    </div>
 </div>
 <div class="cellPhone">
     <img src="{{ asset('7sur7.svg') }}">
@@ -85,11 +87,9 @@
         <p>Le Lay Dev. Web</p>
     </div>
 </footer>
-</footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 
 <script src="{{ asset('script.js') }}"></script>
